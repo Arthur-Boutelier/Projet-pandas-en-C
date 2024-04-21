fa059cc133f5e03d2d3fd1ca47087404966da18e
Arthur <arthur.boutelier@gmail.com>
2024-04-21 16:45:45 +0200

    modif importante changement nom

--- cdataframe.h
+++ cdataframe.h
@@ -6,6 +6,7 @@
 typedef list CDATAFRAME;
 
 CDATAFRAME* creer_cd(TYPE*, int);
+void remplir_cd(CDATAFRAME*);
 void suppr_cd(CDATAFRAME**);
 void suppr_colonne(CDATAFRAME*, char*);
 int nb_colonne_cd(CDATAFRAME*);
@@ -15,8 +16,15 @@ void afficher_colonne_entre(CDATAFRAME*, char*, char*);
 void ajouter_ligne(CDATAFRAME*, list);
 void ajouter_colonne(CDATAFRAME*, COLONNE);
 void suppr_ligne(CDATAFRAME*, long long int);
-void
-
+void renommer_col(CDATAFRAME*, char*, char*);
+int existe_cd(CDATAFRAME*, void*);
+void* obtenir_valeur(CDATAFRAME*, char*, long long int);
+void modif_valeur(CDATAFRAME*, char*, long long int, void*);
+void afficher_nom_col(CDATAFRAME*);
+int nb_ligne_cd(CDATAFRAME*);
+long long int nb_valeur_inf_cd(CDATAFRAME*, void*);
+long long int nb_valeur_sup_cd(CDATAFRAME*, void*);
+long long int nb_valeur_egal_cd(CDATAFRAME*, void*);
 
 
 #endif //PROJET_C_CDATAFRAME_H
