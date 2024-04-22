e82ecd21e96bd3a5dbc7f97f31a51b01edf76316
Victor <victor.proffit@efrei.net>
2024-04-22 10:04:00 +0200

    docstring

--- colonne.h
+++ colonne.h
@@ -36,18 +36,100 @@ typedef struct{
 
 COLONNE* create_column(TYPE type, char * nom);
 int insert_value(COLONNE* col, void*  valeur);
-void delete_column(COLONNE ** col);
+
+/*
+ * Cette fonction permet d'inserer une valeur dans une colonne
+ * Entrée : col : COLONNE* ,Un pointeur visant une colonne
+ *          valeur : void* ,Pointeur visant une valeur
+ * Sortie : Renvoie un booléen celon l'accomplissement de l'insertion
+ */
+void delete_column(COLONNE * col);
+
+/*
+ * Cette fonction permet de supprimer une colonne
+ * Entrée : col : COLONNE**, une double pointeur permettant de viser la colonne à supprimer
+ * Sortie : void
+ */
 void print_col(COLONNE * col);
+
+/*
+ * Cette fonction permet d'afficher une colonne
+ * Entrée : col : COLONNE*, colonne à afficher
+ * Sortie : void
+ */
 void convert_value(COLONNE * col , unsigned long long int indice, char* str, int taille);
+
+/*
+ * Cette fonction permet de convertir la valeur d'une case en str
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ *          indice : long entier non signé
+ *          str : char*
+ *          taille : int
+ * Sortie : void
+ */
 long long int occurence(COLONNE * col, void* valeur);
+/*
+ * Cette fonction permet de calculer le nombre d'occurence d'une valeur dans une colonne
+ * Entrée : col : COLONNE*
+ *          valeur : void*, pointeur visant une valeur
+ * Sortie : long long int
+ */
 char* valeur_pos(COLONNE* col, long long int indice);
+/*
+ * Cette fonction permet de donner la valeur a une indice précis dans une colonne
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ * Sortie : char*
+ */
 long long int nb_valeur_inf_col(COLONNE* col, void* valeur);
+/*
+ * Cette fonction permet de donner le nombre de valeur étant inferieur à la valeur entrée
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ *          valeur : void*, pointeur visant sur une valeur
+ * Sortie : long long int, correspondant au nombre de valeur inferieur à la valeur entrée
+ */
 long long int nb_valeur_sup_col(COLONNE* col, void* valeur);
+/*
+ * Cette fonction permet de donner le nombre de valeur étant inferieur à la valeur entrée
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ *          valeur : void*, pointeur visant sur la valeur
+ * Sortie : long long int, correspondant au nombre de valeur supperieur à la valeur entée
+ */
 long long int nb_valeur_egal_col(COLONNE* col, void* valeur);
+/*
+ * Cette fonction permet de donner le nombre de valeur étant égal à la valeur entrée
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ *          valeur : void*, pointeur visant sur une valeur
+ * Sortie : long long int, correspondant au nombre de valeur égal à la valeur entrée
+ */
 void print_colonne_par_index(COLONNE* col);
+/*
+ * Cette fonction permet d'afficher une colonne trié
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ * Sortie : void
+ */
 void supprimer_index(COLONNE* col);
-int verifier_index(COLONNE* col);
+/*
+ * Cette fonction permet de supprimer les index
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ * Sortie : void
+ */
+int verifier_index(COLONNE* col);/*
+ * Cette fonction permet de vérifier si une colonne a été trié
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ * Sortie : void
+ */
+
 void actualiser_index(COLONNE* col);
+/*
+ * Cette fonction permet de re-trier une colonne ayant déjà été trié par le passé
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ * Sortie : void
+ */
 int chercher_val_col_trier(COLONNE* col, void* valeur);
+/*
+ * Cette fonction permet de rehercher une valeur dans une colonne ayant été trié
+ * Entrée : col : COLONNE*, pointeur visant la colonne
+ * Sortie : int
+ */
 
 #endif
